45da81853195b6fb52185747346f3cad58253278
kongarasan <kongarasan49@gmail.com>
2023-11-23 00:31:37 +0530

    Home Screen

---
+++ UI components/Listcards.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { Text, View, Image, TouchableOpacity } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import LottieView from 'lottie-react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import { useState } from "react";
+const Listcards = ({ item }) => {
+    const [liked, setLiked] = useState(false)
+    const [count,setCount] =useState(0)
+    return (
+        <View style={{ height: 130, width: "90%", backgroundColor: "black", marginTop: 10, marginHorizontal: 20, borderRadius: 20, flexDirection: 'row' }}>
+            <TouchableOpacity 
+            onPress={()=>{setLiked(!liked)}}
+             style={{ position: 'absolute', top: 12, left: 12, zIndex: 4 }}>
+                {
+                    liked ?
+                        <AntDesign name="heart" size={24} color="red" style={{ zIndex: 4 }} />
+                        :
+                        <AntDesign name="heart" size={24} color="white" style={{ zIndex: 4 }} />
+
+                }
+            </TouchableOpacity>
+
+            <Image source={item.image} style={{ height: 130, width: "50%", borderBottomLeftRadius: 20, borderTopLeftRadius: 20 }}></Image>
+            <View style={{ height: 130, width: "50%", borderBottomRightRadius: 20, borderTopRightRadius: 20, backgroundColor: "white" }}>
+                <Text style={{ fontSize: 16, fontWeight: 700, marginTop: 20, textAlign: 'center', lineHeight:16 }}>{item.name}</Text>
+                <Text style={{ fontSize: 16, fontWeight: 600, marginTop: 5, textAlign: 'center', color: '#4CAD73',  lineHeight:16 }}>Rp{item.price}/kg</Text>
+                <Text style={{ fontSize: 14, fontWeight: 600, marginTop: 5, textAlign: 'center', color: '#4CAD73' }}>MRP:Rp {item.mrp}</Text>
+                <View style={{ flexDirection: 'row', alignSelf: 'center', marginTop: 10 }}>
+                    <TouchableOpacity
+                    onPress={()=>{
+                        if(count >0)
+                        {
+                            setCount(count-1)
+                        }
+                        
+                        }}>
+                        <Entypo name="circle-with-minus" size={28} color="#4CAD73" />
+                    </TouchableOpacity>
+                    <View style={{ height: 28, width: 28, backgroundColor: '#DBF4EB', borderRadius: 6, marginHorizontal: 6, alignItems: 'center', justifyContent: 'center' }}>
+                        <Text style={{ color: '#4CAD73', textAlign: 'center' }}>{count}</Text>
+                    </View>
+                    <TouchableOpacity
+                    onPress={()=>{setCount(count+1)}}>
+                        <Entypo name="circle-with-plus" size={28} color="#4CAD73" />
+                    </TouchableOpacity>
+
+
+                </View>
+            </View>
+
+        </View>
+    );
+};
+
+export default Listcards;
--- components/Home.js
+++ components/Home.js
@@ -1,15 +1,179 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, Image, TouchableOpacity, FlatList, SafeAreaView,TextInput } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import LottieView from 'lottie-react-native';
-
+import { MaterialIcons } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
+import { Entypo } from '@expo/vector-icons';
+import Listcards from "../UI components/Listcards";
+import { FontAwesome } from '@expo/vector-icons';
+import { useState, useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
 const Home = () => {
-return (
-	<View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-	<LottieView source={require('../Animation2.json')} autoPlay loop></LottieView>
-	
-	</View>
-);
+	const [text,setText] = useState("")
+
+	const data = [
+		{
+			name: "Fresh Carrot",
+			price: "18,000",
+			mrp: "21,000",
+			image: require("../assets/freshcarrot.png")
+		},
+		{
+			name: "Fresh Red Chilli",
+			price: "12,000",
+			mrp: "14,000",
+			image: require("../assets/chilli.png")
+		},
+		{
+			name: "Fresh Onion",
+			price: "21,000",
+			mrp: "29,000",
+			image: require("../assets/onion.png")
+		},
+		{
+			name: "Fresh Potato",
+			price: "10,000",
+			mrp: "12,000",
+			image: require("../assets/potato.png")
+		},
+		{
+			name: "Fresh Tomato",
+			price: "10,000",
+			mrp: "12,000",
+			image: require("../assets/tomato.png")
+		},
+		{
+			name: "Fresh Carrot",
+			price: "18,000",
+			mrp: "21,000",
+			image: require("../assets/freshcarrot.png")
+		},
+		{
+			name: "Fresh Red Chilli",
+			price: "12,000",
+			mrp: "14,000",
+			image: require("../assets/chilli.png")
+		},
+		{
+			name: "Fresh Onion",
+			price: "21,000",
+			mrp: "29,000",
+			image: require("../assets/onion.png")
+		},
+		{
+			name: "Fresh Potato",
+			price: "10,000",
+			mrp: "12,000",
+			image: require("../assets/potato.png")
+		},
+		{
+			name: "Fresh Tomato",
+			price: "10,000",
+			mrp: "12,000",
+			image: require("../assets/tomato.png")
+		},{
+			name: "Fresh Carrot",
+			price: "18,000",
+			mrp: "21,000",
+			image: require("../assets/freshcarrot.png")
+		},
+		{
+			name: "Fresh Red Chilli",
+			price: "12,000",
+			mrp: "14,000",
+			image: require("../assets/chilli.png")
+		},
+		{
+			name: "Fresh Onion",
+			price: "21,000",
+			mrp: "29,000",
+			image: require("../assets/onion.png")
+		},
+		{
+			name: "Fresh Potato",
+			price: "10,000",
+			mrp: "12,000",
+			image: require("../assets/potato.png")
+		},
+		{
+			name: "Fresh Tomato",
+			price: "10,000",
+			mrp: "12,000",
+			image: require("../assets/tomato.png")
+		},{
+			name: "Fresh Carrot",
+			price: "18,000",
+			mrp: "21,000",
+			image: require("../assets/freshcarrot.png")
+		},
+		{
+			name: "Fresh Red Chilli",
+			price: "12,000",
+			mrp: "14,000",
+			image: require("../assets/chilli.png")
+		},
+		{
+			name: "Fresh Onion",
+			price: "21,000",
+			mrp: "29,000",
+			image: require("../assets/onion.png")
+		},
+		{
+			name: "Fresh Potato",
+			price: "10,000",
+			mrp: "12,000",
+			image: require("../assets/potato.png")
+		},
+		{
+			name: "Fresh Tomato",
+			price: "10,000",
+			mrp: "12,000",
+			image: require("../assets/tomato.png")
+		},
+	]
+
+	const [state, setState] = useState(data)
+
+
+	useEffect(() => {
+        const delayDebounceFn = setTimeout(() => {
+            if (text != '') {
+                setState(data.filter((current)=>current.name.toLocaleLowerCase().includes(text.toLocaleLowerCase())))
+            }else{
+				setState(data)
+			}
+        }, 500)
+        return () => clearTimeout(delayDebounceFn)
+    }, [text])
+
+
+
+	return (
+		<SafeAreaView style={{minHeight:'100%'}}>
+		<StatusBar style="dark"/>
+		<View style={{flexDirection:'row',alignItems:'center',justifyContent:'center', marginTop:50}}>
+		<View style={{  width: 300, marginTop: 10, backgroundColor: "#F5F5F5", borderRadius: 10, alignSelf: 'center', justifyContent: 'center', borderWidth: 1, borderColor: "black" }}>
+                <TextInput style={{ fontSize: 16, color: "#B9B9B9", paddingHorizontal: 10, height: 40, }}
+                    placeholder="Search Item"
+                    onChangeText={Text => setText(Text)}
+                    defaultValue={text}></TextInput>
+				 <FontAwesome name="search" size={24} color="black" style={{position:'absolute',right:10}}/>
+            </View>
+			<TouchableOpacity style={{marginLeft:14,marginTop:4}}>
+			<Entypo name="shopping-cart" size={24} color="black" />
+			</TouchableOpacity>
+			</View>
+
+			<FlatList style={{marginTop:20}}
+				data={state}
+				renderItem={({ item, index }) => (
+					<Listcards item={item}/>
+				)} />
+
+		</SafeAreaView>
+
+	);
 };
 
 export default Home;
